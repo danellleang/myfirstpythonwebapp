94ac6a96a7366de89318d8b052c219c65b3780b2
danellleang <89298987+danellleang@users.noreply.github.com>
2021-09-27 20:29:41 +0800

    Create myapp.py

---
+++ myapp.py
@@ -0,0 +1,108 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@danellleang 
+danellleang
+/
+myfirstapp
+Public
+forked from richieyuyongpoh/myfirstapp
+0
+13
+Code
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+myfirstapp/myfirstapp.py /
+@richieyuyongpoh
+richieyuyongpoh Update myfirstapp.py
+Latest commit 705f8dd on Jul 21
+ History
+ 1 contributor
+64 lines (42 sloc)  1.57 KB
+   
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+import time
+
+st.header("My first Python Web App")
+
+readme = st.checkbox("readme first")
+
+if readme:
+
+    st.write("""
+        This is a web app demo using [streamlit](https://streamlit.io/) library. It is hosted on [heroku](https://www.heroku.com/). You may get the codes via [github](https://github.com/richieyuyongpoh/myfirstapp)
+        """)
+
+    st.write ("For more info, please contact:")
+
+    st.write("<a href='https://www.linkedin.com/in/yong-poh-yu/'>Dr. Yong Poh Yu </a>", unsafe_allow_html=True)
+
+option = st.sidebar.selectbox(
+    'Select a mini project',
+     ['line chart','map','T n C','Long Process'])
+
+
+if option=='line chart':
+    chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+    st.line_chart(chart_data)
+
+elif option=='map':
+    map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+    st.map(map_data)
+
+elif option=='T n C':
+
+    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+    show = st.checkbox('I agree the terms and conditions')
+    if show:
+        st.write(pd.DataFrame({
+        'Intplan': ['yes', 'yes', 'yes', 'no'],
+        'Churn Status': [0, 0, 0, 1]
+        }))
+
+
+else:
+    'Starting a long computation...'
+
+    
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+   
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.1)
+
+    '...and now we\'re done!'
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Loading complete
